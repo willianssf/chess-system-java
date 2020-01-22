421fb5733bc5d6a26624e2a58b9dbfe7fd5ede5f
Willian <willian.silva552@gmail.com>
2020-01-22 09:41:13 -0400

    Chess Excepition and Position

---
+++ src/chess/ChessExcepition.java
@@ -0,0 +1,10 @@
+package chess;
+
+public class ChessExcepition extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ChessExcepition(String msg) {
+		super(msg);		
+	}
+
+}
